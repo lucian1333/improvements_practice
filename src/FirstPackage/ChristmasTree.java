5b19ee0d5dd37ed4e674e7b0a9a8d01698fa3179
lucian1333 <mlmlucian@gmail.com>
2022-05-18 02:34:32 -0500

    this is my own project

---
+++ src/FirstPackage/ChristmasTree.java
@@ -0,0 +1,31 @@
+package FirstPackage;
+
+import java.util.Scanner;
+
+public class ChristmasTree {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Numbers of rows");
+        int n = scan.nextInt();
+
+        for (int i = 1; i<=n; i++) {
+
+            for (int j = 1; j<=n-i; j++)
+
+                System.out.print(" ");
+
+            for (int j = 1; j<=2*i-1; j++)
+
+                System.out.print("*");
+
+
+            System.out.println();
+
+        }
+
+
+
+    }
+}
